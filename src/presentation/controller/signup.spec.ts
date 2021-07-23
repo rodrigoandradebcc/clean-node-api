b967f2da4386900c72cc07f0839dadae39750dc7
Rod Andrade <randrade@brbatel.com.br>
2021-07-23 00:49:08 -0300

    test: ensure SignUpController returns an error if no name is  provided

--- src/presentation/controller/signup.spec.ts
+++ src/presentation/controller/signup.spec.ts
@@ -3,7 +3,7 @@ import { SignUpController } from './signup';
 describe('SignUpController', () => {
   test('Should return 400 if no name is provided', () => {
     const sut = new SignUpController();
-    const hhtpRequest = {
+    const httpRequest = {
       body: {
         name: 'any_name',
         email: 'any_email@mail.com',
@@ -13,5 +13,6 @@ describe('SignUpController', () => {
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   })
 });
