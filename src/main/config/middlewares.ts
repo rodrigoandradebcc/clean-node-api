3f2ad2cd3cc42d95993f3b56d6e36a2eb89b57c1
Rod Andrade <randrade@brbatel.com.br>
2021-08-22 20:49:11 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,7 @@
 import { Express } from 'express'
-import { bodyParser } from '../middlewares/index'
+import { bodyParser, cors } from '../middlewares/index'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
