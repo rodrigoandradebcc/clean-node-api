9a2ab288445ce1d1eafdf89fbf3426fa62f02225
Rod Andrade <randrade@brbatel.com.br>
2021-08-22 21:26:34 -0300

    test: ensure SignUp route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('SignUp Routes', () => {
+  it('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'John Doe',
+        email: 'email@email.com',
+        password: '123',
+        passwordConfirmation: '123'
+
+      })
+      .expect(200)
+  })
+})
