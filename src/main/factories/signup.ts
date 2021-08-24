53424cbaee348b01f51531c16323c00ebc29a558
Rod Andrade <randrade@brbatel.com.br>
2021-08-24 00:42:09 -0300

    feat: add SignUp Factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,16 @@
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BCryptAdapter } from '../../infra/criptography/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { SignUpController } from '../../presentation/controllers/signup/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+
+export const makeSignupController = (): SignUpController => {
+    const salt = 12
+    
+    const emailValidatorAdapter = new EmailValidatorAdapter()
+    const bcryptAdapter = new BCryptAdapter(salt)
+    const accountMongoRepository = new AccountMongoRepository()
+    const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+
+    return new SignUpController(emailValidatorAdapter, dbAddAccount)
+}
